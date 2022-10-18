b380f03fbc8b4e96ee9513b1dc298962f4f07b5f
cuong <anhcuonggt@gmail.com>
2022-10-18 16:36:41 +0700

    update

--- src/Js/header.js
+++ src/Js/header.js
@@ -1,5 +1,4 @@
 function tabSubmenu() {
-  console.log($(".sub-nav-item"));
   $(".sub-nav-item").on("click", function () {
     if (this.classList.contains("sub-nav-item4")) {
       return;
@@ -24,30 +23,41 @@ function showSubMenu() {
 }
 showSubMenu();
 function StopPropagationDropdownMenu() {
-  $(".dropdown-menu-full ").on("click", (e) => {
+  $(".dropdown-menu-full").on("click", (e) => {
     e.stopPropagation();
   });
 }
 StopPropagationDropdownMenu();
 
 function ShowSubMenu() {
-  $("#submenu-dropdown-mobile-2").toggleClass(
-    "submenu-dropdown-mobile-2-active"
-  );
+  $(".menu-ranks-1").on("click", function () {
+    $(this)
+      .parent()
+      .find(".menu-ranks-1-content")
+      .addClass("menu-ranks-content-active");
+  });
 }
+ShowSubMenu();
 function showSubMenu2() {
   $(".menu-ranks-2").on("click", function () {
-    $(this).next().toggleClass("menu-ranks-2-content");
+    $(this).next().toggleClass("menu-ranks-content-active");
   });
 }
 showSubMenu2();
 function showSubMenu3() {
   $(".menu-ranks-3").on("click", function (e) {
     e.stopPropagation();
-    $(this).next().toggleClass("menu-ranks-3-content");
+    $(this).next().toggleClass("menu-ranks-content-active");
   });
 }
 showSubMenu3();
+function showSubMenu4() {
+  $(".parent-menu-2").on("click", function (e) {
+    e.stopPropagation();
+    $(".parent-menu-2__submenu").toggleClass("menu-ranks-content-active");
+  });
+}
+showSubMenu4();
 
 function showSubMenuMobile() {
   $(".bar-icon").on("click", function () {
@@ -55,19 +65,30 @@ function showSubMenuMobile() {
   });
 }
 showSubMenuMobile();
-function hideSubMenu(classSubMenu, classBtnShow, classActive) {
-  const ElmMenuMobile = document.querySelector(classSubMenu);
-  const ElmBtnShowMenuMobile = document.querySelector(classBtnShow);
-  window.addEventListener("click", function (e) {
-    if (
-      e.target.closest(classSubMenu) !== ElmMenuMobile &&
-      e.target.closest(classBtnShow) !== ElmBtnShowMenuMobile
-    ) {
-      $(ElmMenuMobile).removeClass(classActive);
-    } else {
-      $(".parent-menu").addClass(classActive);
+function hideSubMenu() {
+  $(".menu-ranks-icon").on("click", function () {
+    $(this).parent().parent().parent().removeClass("menu-ranks-content-active");
+    $(this).parent().parent().removeClass("menu-ranks-content-active");
+  });
+}
+hideSubMenu();
+function touchEventLeft() {
+  let x;
+  let y;
+  window.addEventListener("touchstart", function (e) {
+    x = e.touches[0].clientX;
+  });
+  window.addEventListener("touchmove", function (e) {
+    y = e.touches[0].clientX;
+  });
+  window.addEventListener("touchend", function (e) {
+    if (y - x >= 50) {
+      $(e.target.closest(".menu-ranks-content-active")).removeClass(
+        "menu-ranks-content-active"
+      );
+      x = 0;
+      y = 0;
     }
   });
 }
-hideSubMenu(".parent-menu", ".bar-icon", "parent-menu-active");
-hideSubMenu(".dropdown-menu-full", ".nav__menu-item", "sub-menu-active");
+touchEventLeft();
